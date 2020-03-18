6ba9911fe3d342b48b1477c9b693e62fbde563ed
Kevin Hall <kevinmhall8@gmail.com>
2020-03-18 14:44:06 -0700

    inquirer working

--- app.js
+++ app.js
@@ -4,30 +4,118 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+function createTeam() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeType",
+            message: "Select Employee Type",
+            choices: ["Engineer", "Manager", "Intern"]
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the email address?"
+        }
+    ]).then((data) => {
+        const { employeeType } = data
+        switch (employeeType) {
+            case "Engineer":
+                createEngineer(data)
+                break;
+            case "Manager":
+                createManager(data)
+                break;
+            case "Intern":
+                createIntern(data)
+                break;
+
+            default:
+                break;
+        }
+    })
+
+}
+function createEngineer(employeeInfo) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "What is the GitHub?"
+        }
+    ]).then((data) => {
+        const { name, id, email } = employeeInfo
+        const { github } = data
+
+        let newEngineer = new Engineer (name, id, email, github)
+        console.log(newEngineer)
+    })
+}
+
+function createManager(employeeInfo) {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the office number?"
+    }
+    ]).then((data) => {
+        const {name, id, email} = employeeInfo
+        const { officeNumber } = data
+
+        let newManager = new Manager (name, id, email, officeNumber)
+        console.log(newManager)
+    })
+}
+
+function createIntern(employeeInfo) {
+    inquirer.prompt([
+    {
+        type: "input",
+        name: "school",
+        message: "What is the school?"
+    }
+]).then((data) => {
+    const {name, id, email} = employeeInfo
+    const { school } = data
+
+    let newIntern = new Intern (name, id, email, school)
+    console.log(newIntern)
+})
+}
+
+createTeam()
+
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
